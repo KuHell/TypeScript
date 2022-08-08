a40dbb395b7d315a78b251b050ff55c48ac4aadb
KuHell <itkdgus489@gmail.com>
2022-08-08 11:17:35 +0900

    type지정 에러 코드

---
+++ TypescriptWorks.ts
@@ -0,0 +1,11 @@
+let b :boolean = 'x' //타입: boolean (true/false)
+let c :boolean = false 
+
+let d :number[] = []
+d.push(1)
+d.push('error')
+
+const player = {
+  name: 'kuhell'
+}
+player.hello()
