a6041f068093bb3d80b5b9392c4c81a602b5bcfa
KuHell <itkdgus489@gmail.com>
2022-08-08 14:03:44 +0900

    never void

--- types_of_TSThree.ts
+++ types_of_TSThree.ts
@@ -7,3 +7,19 @@ if(typeof a === 'string') {
   let b = a.toUpperCase();
 }
 
+//void : 아무것도 return하지 않는 함수
+function hello() {
+  console.log('x')
+}
+const y = hello()
+
+//never
+function hello2(name :string | number) {
+  if(typeof name === 'string') {
+    name
+  }else if (typeof name ==='number') {
+    name
+  }else {
+    name
+  }
+}
